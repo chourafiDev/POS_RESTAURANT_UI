aed58de1564680117c8d8da3d4f62f510a3bc985
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-05-29 00:01:10 +0100

    Design sidebar component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,91 @@
-import React from 'react'
+"use client";
+
+import { useState, useMemo } from "react";
+import classNames from "classnames";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+  const menuItems = [
+    {
+      id: 1,
+      title: "Home",
+      link: "/",
+    },
+    {
+      id: 2,
+      title: "Users",
+      link: "/users",
+    },
+    {
+      id: 3,
+      title: "About",
+      link: "/about",
+    },
+    {
+      id: 4,
+      title: "Setting",
+      link: "/setting",
+    },
+  ];
 
-export default Sidebar
+  const [toggleCollaps, setToggleCollapse] = useState(false);
+  const router = useRouter();
+
+  const activeMenu = useMemo(
+    () => menuItems.find(({ link }) => link === router.pathname),
+    [router.pathname]
+  );
+
+  const wrapperClasses = classNames(
+    "h-screen flex flex-col justify-between p-4 bg-white",
+    {
+      ["w-72"]: !toggleCollaps,
+      ["w-20"]: toggleCollaps,
+    }
+  );
+
+  const getNavItmsClasses = (menu: string) => {
+    return classNames(
+      "flex items-center cursor-pointer hover:bg-white rounded w-full overflow-hidden whitespace-nowrap",
+      { ["bg-brand/30"]: activeMenu?.title === menu }
+    );
+  };
+
+  const hundleToggle = () => {
+    setToggleCollapse(!toggleCollaps);
+  };
+
+  return (
+    <div
+      className={wrapperClasses}
+      style={{ transition: "width 300ms cubic-bezier(0.2, 0, 0, 1) 0s" }}
+    >
+      <div>
+        {/* Logo */}
+        <div className="flex items-center justify-between">
+          <p>Logo</p>
+          <button onClick={hundleToggle}>toggle</button>
+        </div>
+
+        {/* Navigation items */}
+        <ul className="flex flex-col gap-2 mt-24">
+          {menuItems.map(({ id, title, link }) => {
+            const classes = getNavItmsClasses(title);
+            return (
+              <li className={classes} key={id}>
+                <Link href={link}>{title}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div>
+        <p>Logout</p>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
