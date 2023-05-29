9c802f366b2b5e2df8b02f00509efd4f0a65c7b2
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-05-29 21:11:43 +0100

    Desigh login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import Form from "@/components/login/Form";
+import SwiperSlider from "@/components/login/SwiperSlider";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="grid grid-cols-3">
+      <div>
+        <SwiperSlider />
+      </div>
+      <div>
+        <Form />
+      </div>
+    </div>
+  );
 };
 
 export default page;
