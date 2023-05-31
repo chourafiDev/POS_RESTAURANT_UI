207886f2fe83f526c95d2023a988ab1837fb22b2
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-05-31 18:00:41 +0100

    Add folder utils

---
+++ src/utils/data.ts
@@ -0,0 +1,50 @@
+export const data = [
+  {
+    id: 1,
+    name: "All",
+    icon: "dish.png",
+    total: 200,
+  },
+  {
+    id: 2,
+    name: "Burger",
+    icon: "burger.png",
+    total: 5,
+  },
+  {
+    id: 3,
+    name: "Juice",
+    icon: "juice.png",
+    total: 5,
+  },
+  {
+    id: 4,
+    name: "Salad",
+    icon: "tacos.png",
+    total: 5,
+  },
+  {
+    id: 5,
+    name: "Tacos",
+    icon: "tacos.png",
+    total: 50,
+  },
+  {
+    id: 6,
+    name: "Dessert",
+    icon: "dessert.png",
+    total: 50,
+  },
+  {
+    id: 7,
+    name: "Pizza",
+    icon: "pizza.png",
+    total: 50,
+  },
+  {
+    id: 8,
+    name: "Chicken ",
+    icon: "chicken.png",
+    total: 50,
+  },
+];
