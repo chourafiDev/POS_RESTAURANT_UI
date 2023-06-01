8f462cf7896397367f64ded63d797a6e0feb1446
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-06-01 11:28:28 +0100

    Fix design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import Navbar from "@/components/Navbar/Navbar";
+import CategoriesList from "../components/CategoriesList/CategoriesList ";
+import { data } from "@/utils/data";
+
 export default function Home() {
-  return <div className="">Home</div>;
+  return (
+    <div className="">
+      <Navbar />
+
+      {/* Categories */}
+      <div className="px-8">
+        <h2 className="text-dark font-medium text-lg mb-6">Categories</h2>
+        <CategoriesList data={data} />
+      </div>
+    </div>
+  );
 }
