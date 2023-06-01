0032d35cb8fa2a0a988df52be752ec6af3e0372b
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-06-01 17:59:14 +0100

    Design menu item

--- src/components/menu/MenuItem.tsx
+++ src/components/menu/MenuItem.tsx
@@ -1,27 +1,36 @@
 import { FC } from "react";
 import { Menu } from "../../../types";
 import Image from "next/image";
+import { useDisclosure } from "@mantine/hooks";
+import Cart from "../cart/Cart";
 
 interface MenuItemProps {
   menu: Menu;
 }
 
 const MenuItem: FC<MenuItemProps> = ({ menu: { title, price, image } }) => {
-  return (
-    <div>
-      <Image
-        src={`/assets/imgs/menu/${image}`}
-        alt={title}
-        width={300}
-        height={100}
-        className="rounded-md"
-      />
+  const [opened, { open, close }] = useDisclosure(false);
 
-      <div className="mt-2">
-        <h1 className="text-gray/80 mb-2 text-[14px] font-normal tracking-wide">{title}</h1>
-        <strong className="text-dark font-medium">${price}</strong>
+  return (
+    <>
+      <div onClick={open} className="cursor-pointer">
+        <Image
+          src={`/assets/imgs/menu/${image}`}
+          alt={title}
+          width={300}
+          height={100}
+          className="rounded-md"
+        />
+
+        <div className="mt-2">
+          <h1 className="text-gray/80 mb-2 text-[14px] font-normal tracking-wide">
+            {title}
+          </h1>
+          <strong className="text-dark font-medium">${price}</strong>
+        </div>
       </div>
-    </div>
+      <Cart opened={opened} close={close} />
+    </>
   );
 };
 
