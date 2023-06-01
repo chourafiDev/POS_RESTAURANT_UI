eefe3d0c5bb7c192813f617e79597ef4c03a61bc
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-06-01 11:21:45 +0100

    Fix design

--- src/components/CategoriesList/CategoryItem.tsx
+++ src/components/CategoriesList/CategoryItem.tsx
@@ -9,12 +9,10 @@ interface CategoryItemProps {
 const Index: FC<CategoryItemProps> = ({ category: { name, icon, total } }) => {
   return (
     <div className="border border-gray-light rounded-md py-3 px-3 shadow-zinc-500 inline-block w-28">
-      <Image
-        src={`/assets/imgs/icons/${icon}`}
-        width={50}
-        height={50}
-        alt={name}
-      />
+      <div style={{ width: 30, height: 30 }}>
+        <img src={`/assets/imgs/icons/${icon}`} alt={name} className="w-full h-full" />
+      </div>
+
       <p className="mt-3 text-gray/60 text-sm font-medium">{name}</p>
       <p className="text-dark/80 text-[14px] font-medium mt-1">{total} item</p>
     </div>
