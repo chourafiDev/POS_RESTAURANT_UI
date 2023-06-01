08fc6b9356710a099da8228b8e7d19fe2e4856c5
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-06-01 12:20:08 +0100

    Add folder menu component

---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Menu = () => {
+  return (
+    <div className="px-8 mt-6">
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="text-dark font-medium text-xl mb-3">Special menu for you</h2>
+
+        <div className="flex gap-3 items-center">
+          {/* <div
+            ref={prevRef}
+            className="w-9 h-9 flex cursor-pointer justify-center items-center rounded-full border border-gray/30 bg-gray-light/10 text-gray"
+          >
+            <FiChevronLeft />
+          </div>
+          <div
+            ref={nextRef}
+            className="w-9 h-9 flex cursor-pointer justify-center items-center rounded-full border border-gray/30 bg-gray-light/10 text-gray"
+          >
+            <FiChevronRight />
+          </div> */}
+        </div>
+      </div>
+
+
+      <div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
