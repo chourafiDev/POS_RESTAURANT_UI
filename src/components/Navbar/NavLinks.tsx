7d81dc5c5d27e49050ad15b30fc7dc7f8733a7b1
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-06-02 12:48:17 +0100

    Add icon to nav links

--- src/components/Navbar/NavLinks.tsx
+++ src/components/Navbar/NavLinks.tsx
@@ -1,19 +1,33 @@
 import Link from "next/link";
+import { TbLayout2, TbClockHour9, TbNotebook } from "react-icons/tb";
+import { RiFileList3Line } from "react-icons/ri";
 
 const NavLinks = () => {
   return (
     <ul className="flex items-center gap-x-8 text-gray">
-      <li className="text-md font-normal hover:text-brand duration-200 ease-in">
-        <Link href="/">Reservation</Link>
+      <li className="text-[15px] font-normal hover:text-brand duration-200 ease-in">
+        <Link href="/" className="flex items-center gap-2">
+          <TbLayout2 size={18} />
+          <p>Dashboard</p>
+        </Link>
       </li>
-      <li className="text-md font-normal hover:text-brand duration-200 ease-in">
-        <Link href="/">Menu</Link>
+      <li className="text-[15px] font-normal hover:text-brand duration-200 ease-in">
+        <Link href="/" className="flex items-center gap-2">
+          <TbNotebook size={18} />
+          <p>Order List</p>
+        </Link>
       </li>
-      <li className="text-md font-normal hover:text-brand duration-200 ease-in">
-        <Link href="/">Delivery</Link>
+      <li className="text-[15px] font-normal hover:text-brand duration-200 ease-in">
+        <Link href="/" className="flex items-center gap-2">
+          <TbClockHour9 size={18} />
+          <p>History</p>
+        </Link>
       </li>
-      <li className="text-md font-normal hover:text-brand duration-200 ease-in">
-        <Link href="/">Accounting</Link>
+      <li className="text-[15px] font-normal hover:text-brand duration-200 ease-in">
+        <Link href="/" className="flex items-center gap-2">
+          <RiFileList3Line size={18} />
+          <p>Bills</p>
+        </Link>
       </li>
     </ul>
   );
