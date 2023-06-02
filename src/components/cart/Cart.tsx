2bae8a531dd03fa57a302543cd95c600444ffe3e
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-06-02 16:59:40 +0100

    Fix design

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -18,25 +18,30 @@ const Cart = ({ opened, close }: CartProps) => {
       withCloseButton={false}
       overlayProps={{ opacity: 0.5, blur: 4 }}
     >
-      <div>
-        <div className="border-b border-gray-light pb-3">
-          <h2 className="text-dark font-medium mb-1 text-lg">Current Order</h2>
-          <p className="text-gray text-sm font-normal">
-            Table (T1, T2, T6, T8)
-          </p>
+      <div className="max-h-screen flex flex-col justify-between gap-3">
+        <div className="flex-1 overflow-y-scroll">
+          <div className="border-b border-gray-light pb-3">
+            <h2 className="text-dark font-medium mb-1 text-lg">
+              Current Order
+            </h2>
+            <p className="text-gray text-sm font-normal">
+              Table (T1, T2, T6, T8)
+            </p>
+          </div>
+
+          <div className="space-y-4 mt-4">
+            {oredrs.slice(0, 4).map((order) => (
+              <Order key={order.id} order={order} />
+            ))}
+          </div>
+
+          <Bill />
         </div>
-
-        <div className="space-y-4 mt-4">
-          {oredrs.slice(0, 4).map((order) => (
-            <Order key={order.id} order={order} />
-          ))}
+        <div className="flex-1 mb-6">
+          <Button variant="default" size="default" className="">
+            Charge $93.24
+          </Button>
         </div>
-
-        <Bill />
-
-        <Button variant="default" size="default" className="mt-8">
-          Charge $93.24
-        </Button>
       </div>
     </Drawer>
   );
