1c8500c946b08fa126f8f2c878608e283b9018e0
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-06-05 14:40:57 +0100

    Ignore navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Categories from "../components/Categories/Categories";
 import { categories } from "@/utils/data";
 import Menu from "@/components/menu/Menu";
-import Navbar from "@/components/navbar/Navbar";
+// import Navbar from "@/components/navbar/Navbar";
 
 export default function Home() {
   return (
     <div className="">
-      <Navbar />
+      {/* <Navbar /> */}
 
       {/* Categories */}
       <Categories categories={categories} />
