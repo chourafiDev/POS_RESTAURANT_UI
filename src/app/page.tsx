fee576d6f786b811c4cf60df2b26ee0a39453c6d
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-06-10 22:24:29 +0100

    Fix design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,5 @@ import { categories } from "@/utils/data";
 import Menu from "@/components/menu/Menu";
 
 export default function Home() {
-  return (
-    <div className="px-4 md:px-6 bg-[#fafafa]">
-      {/* Categories */}
-      <Categories categories={categories} />
-
-      {/* Menu */}
-      <Menu />
-    </div>
-  );
+  return <div className="px-4 md:px-6 bg-[#fafafa]">Dashboard</div>;
 }
