7e434760b2d42c60ba6b38e4d0289e0c6671dceb
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-06-13 15:51:28 +0100

    Fix design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,24 @@
 import Header from "@/components/dashbord/Header";
+import Income from "@/components/dashbord/Income";
+import OutOfStock from "@/components/dashbord/OutOfStock";
 import Revenue from "@/components/dashbord/Revenue";
 import Sales from "@/components/dashbord/Sales";
+import Trending from "@/components/dashbord/Trending";
 
 export default function Home() {
   return (
-    <div className="px-4 md:px-6 bg-[#fafafa] min-h-screen">
+    <div className="px-4 md:px-6 py-6 bg-[#fafafa] min-h-screen">
       <Header />
       <Revenue />
 
       <div className="flex items-center gap-4 mt-8">
         <Sales />
+        <Income />
+      </div>
+
+      <div className="flex items-center gap-4 mt-8">
+        <Trending />
+        <OutOfStock />
       </div>
     </div>
   );
