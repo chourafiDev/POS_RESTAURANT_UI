3901a18b85256784b972fcf88692a9e4e0af23ae
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-06-14 11:30:01 +0100

    Add folder menu for design components to menu page

---
+++ src/app/menu/page.tsx
@@ -0,0 +1,19 @@
+import Categories from "../../components/Categories/Categories";
+import { categories } from "@/utils/data";
+import Menu from "@/components/menu/Menu";
+import Search from "@/components/Search";
+
+export default function Order() {
+  return (
+    <div className="p-4 md:p-6 bg-[#fafafa]">
+      {/* Search */}
+      <Search />
+
+      {/* Categories */}
+      <Categories categories={categories} />
+
+      {/* Menu */}
+      <Menu />
+    </div>
+  );
+}
