2663467f91f701e03048332048624d4f81f44423
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-06-17 17:22:27 +0100

    Add settings page

---
+++ src/app/(app)/setting/page.tsx
@@ -0,0 +1,26 @@
+import Settings from "@/components/settings/Settings";
+import Categories from "@/components/settings/categories/Categories";
+import Menu from "@/components/settings/menu/Menu";
+
+const tabs = [
+  {
+    title: "Menu",
+    id: "menu",
+    content: Menu,
+  },
+  {
+    title: "Categories",
+    id: "categories",
+    content: Categories,
+  },
+];
+
+const page = () => {
+  return (
+    <div className="p-4 md:p-6 bg-[#fafafa]">
+      <Settings tabs={tabs} />
+    </div>
+  );
+};
+
+export default page;
