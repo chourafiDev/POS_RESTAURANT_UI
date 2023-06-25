e959489d62cd05576688ad6446d8d0f60e224c17
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-06-25 18:53:14 +0100

    Add apiSlice middleware

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./features/authSlice";
+import { apiSlice } from "./services/apiSlice";
 
 export const store = configureStore({
-  reducer: {},
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  reducer: {
+    auth: authReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: process.env.NODE_ENV !== "production",
 });
 
