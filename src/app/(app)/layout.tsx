d8514b54ef518329e5a99967d78235889c0f178c
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-07-04 17:50:57 +0100

    Fix layout design

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,8 +1,15 @@
+"use client";
+
 import "../globals.css";
 import { Poppins, Inter } from "next/font/google";
-import LayoutContext from "@/components/app/LayoutContext";
 import { Providers } from "@/redux/provider";
-import { message } from "antd";
+import { ConfigProvider } from "antd";
+import { Transition } from "@headlessui/react";
+
+import { useState, useEffect, Fragment } from "react";
+import NextNProgress from "nextjs-progressbar";
+import TopBar from "@/components/app/TopBar";
+import SideBar from "@/components/app/SideBar";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -21,14 +28,69 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const [messageApi, contextHolder] = message.useMessage();
+  const [showNav, setShowNav] = useState<boolean>(true);
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  function handleResize() {
+    if (innerWidth <= 640) {
+      setShowNav(false);
+      setIsMobile(true);
+    } else {
+      setShowNav(true);
+      setIsMobile(false);
+    }
+  }
+
+  useEffect(() => {
+    if (typeof window != undefined) {
+      addEventListener("resize", handleResize);
+    }
+
+    return () => {
+      removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <html lang="en">
       <body className={inter.className}>
         <Providers>
-          {contextHolder}
-          <LayoutContext>{children}</LayoutContext>
+          <ConfigProvider
+            theme={{
+              token: {
+                colorPrimary: "#46A094",
+              },
+            }}
+          >
+            <NextNProgress
+              color="#6591ff"
+              startPosition={0.3}
+              stopDelayMs={200}
+              height={3}
+              showOnShallow={true}
+              options={{ showSpinner: false }}
+            />
+            <TopBar showNav={showNav} setShowNav={setShowNav} />
+            <Transition
+              as={Fragment}
+              show={showNav}
+              enter="transform transition duration-[400ms]"
+              enterFrom="-translate-x-full"
+              enterTo="translate-x-0"
+              leave="transform duration-[400ms] transition ease-in-out"
+              leaveFrom="translate-x-0"
+              leaveTo="-translate-x-full"
+            >
+              <SideBar showNav={showNav} />
+            </Transition>
+            <main
+              className={`h-screen pt-16 transition-all duration-[400ms] bg-white ${
+                showNav && !isMobile ? "pl-24" : ""
+              }`}
+            >
+              {children}
+            </main>
+          </ConfigProvider>
         </Providers>
       </body>
     </html>
