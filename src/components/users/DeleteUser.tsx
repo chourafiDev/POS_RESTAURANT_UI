c8b04add3307906e41080bdf403388f52169848c
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-07-04 17:53:56 +0100

    Add modal antd

--- src/components/users/DeleteUser.tsx
+++ src/components/users/DeleteUser.tsx
@@ -1,62 +1,53 @@
 "use client";
 
-import { Modal, useMantineTheme } from "@mantine/core";
 import { FC } from "react";
 import { BsTrash } from "react-icons/bs";
 import Button from "../ui/Button";
+import { Modal } from "antd";
 
 interface DeleteUserProps {
-  modalDeleteOpened: boolean;
-  closeModalDelete: () => void;
+  openModalDelete: boolean;
+  handleCloseModal: (type: string) => void;
 }
 
 const DeleteUser: FC<DeleteUserProps> = ({
-  modalDeleteOpened,
-  closeModalDelete,
+  openModalDelete,
+  handleCloseModal,
 }) => {
-  const theme = useMantineTheme();
   return (
     <Modal
-      opened={modalDeleteOpened}
-      onClose={closeModalDelete}
-      withCloseButton={false}
+      title=""
       centered
-      overlayProps={{
-        color:
-          theme.colorScheme === "dark"
-            ? theme.colors.dark[9]
-            : theme.colors.gray[2],
-        opacity: 0.55,
-        blur: 3,
-      }}
-      size="lg"
+      open={openModalDelete}
+      onOk={() => handleCloseModal("delete")}
+      onCancel={() => handleCloseModal("delete")}
+      width={600}
+      footer={[]}
     >
-      <div>
-        <div className="py-8">
-          <BsTrash className="mx-auto text-red mb-2" size={24} />
-          <h2 className="text-red text-center font-medium text-xl">
-            Delete user
-          </h2>
-        </div>
-        <div className="bg-gray-light/30 px-28 py-8 border-t border-gray/20">
-          <p className="text-dark/80 mb-5 font-medium text-[15px] leading-6">
-            Deleting a user will permanently remove it from your application
-          </p>
+      <div className="py-8">
+        <BsTrash className="mx-auto text-red mb-2" size={24} />
+        <h2 className="text-red text-center font-medium text-xl">
+          Delete user
+        </h2>
+      </div>
+      <div className="bg-gray-light/30 px-28 py-8 border-t border-gray/20">
+        <p className="text-dark/80 mb-5 font-medium text-[15px] leading-6">
+          Deleting a user will permanently remove it from your application
+        </p>
 
-          <div className="flex items-center gap-3">
-            <Button
-              variant="outline-gray"
-              size="default"
-              rounded="default"
-              className="bg-white"
-              onClick={closeModalDelete}
-            >
-              No, Keep User
-            </Button>
-            <Button variant="destructive" size="default" rounded="default">
-              Yes, Delete User
-            </Button>
-          </div>
+        <div className="flex items-center gap-3">
+          <Button
+            variant="outline-gray"
+            size="default"
+            rounded="default"
+            className="bg-white"
+            onClick={() => handleCloseModal("delete")}
+          >
+            No, Keep User
+          </Button>
+          <Button variant="destructive" size="default" rounded="default">
+            Yes, Delete User
+          </Button>
         </div>
       </div>
     </Modal>
