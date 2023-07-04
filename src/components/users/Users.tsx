bfda69af7c12a496c67c69eef93de2ce0cfb73cd
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-07-04 17:54:21 +0100

    Add table antd

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -1,26 +1,58 @@
 "use client";
 
-import { useState } from "react";
-import { users } from "@/utils/data";
-import { ImSearch } from "react-icons/im";
+import { useState, useRef } from "react";
 import { RiDeleteBin5Fill } from "react-icons/ri";
 import { LuPlusCircle } from "react-icons/lu";
 import { AiFillEdit } from "react-icons/ai";
 import { FaEye } from "react-icons/fa";
 import DeleteUser from "./DeleteUser";
-import { useDisclosure } from "@mantine/hooks";
 import AddUser from "./AddUser";
-import Button from "../ui/Button";
 import EditUser from "./EditUser";
 import DetailsUser from "./DetailsUser";
+import Button from "@/components/ui/Button";
+import { SearchOutlined } from "@ant-design/icons";
+import type { InputRef } from "antd";
+import { Input, Space, Table } from "antd";
+import type { ColumnType, ColumnsType } from "antd/es/table";
+import type { FilterConfirmProps } from "antd/es/table/interface";
+import Highlighter from "react-highlight-words";
+
+interface DataType {
+  key: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+type DataIndex = keyof DataType;
+
+const data: DataType[] = [
+  {
+    key: "1",
+    firstName: "John",
+    lastName: "Brown",
+    email: "jhon@gmail.com",
+  },
+  {
+    key: "2",
+    firstName: "Joe",
+    lastName: "Black",
+    email: "joe@gmail.com",
+  },
+  {
+    key: "3",
+    firstName: "Jim",
+    lastName: "Green",
+    email: "jim@gmail.com",
+  },
+];
 
 const Users = () => {
-  // handle modal add user
-  // const [modalAddOpened, { open: openModalAdd, close: closeModalAdd }] =
-  //   useDisclosure(false);
-
+  // manage modal
   const [openModalAdd, setOpenModalAdd] = useState(false);
   const [openModalEdit, setOpenModalEdit] = useState(false);
+  const [openModalDelete, setOpenModalDelete] = useState(false);
+  const [openModalDetail, setOpenModalDetail] = useState(false);
 
   function handleOpneModal(type: string) {
     switch (type) {
@@ -29,6 +61,12 @@ const Users = () => {
         break;
       case "edit":
         setOpenModalEdit(true);
+        break;
+      case "delete":
+        setOpenModalDelete(true);
+        break;
+      case "detail":
+        setOpenModalDetail(true);
         break;
 
       default:
@@ -43,147 +81,218 @@ const Users = () => {
       case "edit":
         setOpenModalEdit(false);
         break;
+      case "delete":
+        setOpenModalDelete(false);
+        break;
+      case "detail":
+        setOpenModalDetail(false);
+        break;
 
       default:
         break;
     }
   }
 
-  //   const handleAddUser = (user) => {};
+  // handle table data
+  const [searchText, setSearchText] = useState("");
+  const [searchedColumn, setSearchedColumn] = useState("");
+  const searchInput = useRef<InputRef>(null);
 
-  // handle edit user
-  // const [modalEditOpened, { open: openModalEdit, close: closeModalEdit }] =
-  //   useDisclosure(false);
+  const handleSearch = (
+    selectedKeys: string[],
+    confirm: (param?: FilterConfirmProps) => void,
+    dataIndex: DataIndex
+  ) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
 
-  // handle details user
-  const [
-    modalDetailsOpened,
-    { open: openModalDetails, close: closeModalDetails },
-  ] = useDisclosure(false);
+  const handleReset = (clearFilters: () => void) => {
+    clearFilters();
+    setSearchText("");
+  };
 
-  // handle delete user
-  const [
-    modalDeleteOpened,
-    { open: openModalDelete, close: closeModalDelete },
-  ] = useDisclosure(false);
+  const getColumnSearchProps = (
+    dataIndex: DataIndex
+  ): ColumnType<DataType> => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+      close,
+    }) => (
+      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() =>
+            handleSearch(selectedKeys as string[], confirm, dataIndex)
+          }
+          style={{ marginBottom: 8, display: "block" }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() =>
+              handleSearch(selectedKeys as string[], confirm, dataIndex)
+            }
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({ closeDropdown: false });
+              setSearchText((selectedKeys as string[])[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered: boolean) => (
+      <SearchOutlined style={{ color: filtered ? "#1677ff" : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes((value as string).toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ""}
+        />
+      ) : (
+        text
+      ),
+  });
 
-  //   const handleDleteUserById = (userId) => {};
+  const columns: ColumnsType<DataType> = [
+    {
+      title: "First Name",
+      dataIndex: "firstName",
+      key: "firstName",
+      width: "30%",
+      ...getColumnSearchProps("firstName"),
+      sorter: (a, b) => a.firstName.length - b.firstName.length,
+      sortDirections: ["descend", "ascend"],
+    },
+    {
+      title: "Last Name",
+      dataIndex: "lastName",
+      key: "lastName",
+      width: "20%",
+      ...getColumnSearchProps("lastName"),
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Actions",
+      key: "actions",
+      render: (_, record) => (
+        <Space size="middle">
+          <button
+            onClick={() => handleOpneModal("detail")}
+            className="w-9 h-9 rounded-md bg-brand/30 text-brand flex justify-center items-center"
+          >
+            <FaEye size={16} />
+          </button>
+          <button
+            onClick={() => handleOpneModal("edit")}
+            className="w-9 h-9 rounded-md bg-yellow/30 text-yellow flex justify-center items-center"
+          >
+            <AiFillEdit size={16} />
+          </button>
+          <button
+            onClick={() => handleOpneModal("delete")}
+            className="w-9 h-9 rounded-md bg-red/30 text-red flex justify-center items-center"
+          >
+            <RiDeleteBin5Fill size={16} />
+          </button>
+        </Space>
+      ),
+    },
+  ];
 
   return (
     <>
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between mb-3">
         <h1 className="text-dark font-semibold mb-5 text-[18px]">List Users</h1>
         <div className="inline-block">
           <Button
             variant="default"
             size="default"
             rounded="full"
-            className="gap-3"
+            className="flex items-center gap-2"
             onClick={() => handleOpneModal("add")}
           >
+            <LuPlusCircle />
             Add User
-            <LuPlusCircle size={20} />
           </Button>
         </div>
       </div>
 
-      <div className="flex items-center bg-gray-light/60 py-[3px] px-4 gap-2 rounded-xl w-full my-6">
-        <ImSearch className="text-dark/40" size={17} />
-        <input
-          type="text"
-          className="outline-none px-2 py-3 font-normal bg-transparent text-sm text-gray w-full"
-          placeholder="Search menu"
-        />
-      </div>
-
-      <div className="flex flex-col">
-        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-            <div className="overflow-hidden">
-              <table className="min-w-full text-left text-sm font-light">
-                <thead className="border-b font-medium border-gray/10">
-                  <tr>
-                    <th scope="col" className="px-6 py-4 text-dark">
-                      First Name
-                    </th>
-                    <th scope="col" className="px-6 py-4 text-dark">
-                      Last Name
-                    </th>
-                    <th scope="col" className="px-6 py-4 text-dark">
-                      Phone
-                    </th>
-                    <th scope="col" className="px-6 py-4 text-dark">
-                      Email
-                    </th>
-                    <th scope="col" className="px-6 py-4 text-dark">
-                      Action
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {users.map((user) => (
-                    <tr key={user.id} className="border-b border-gray/10">
-                      <td className="whitespace-nowrap px-6 py-4 font-medium text-dark/70">
-                        {user.firstName}
-                      </td>
-                      <td className="whitespace-nowrap px-6 py-4 font-medium text-dark/70">
-                        {user.lastName}
-                      </td>
-                      <td className="whitespace-nowrap px-6 py-4 font-medium text-dark/70">
-                        {user.phone}
-                      </td>
-                      <td className="whitespace-nowrap px-6 py-4 font-medium text-dark/70">
-                        {user.email}
-                      </td>
-
-                      <td className="flex items-center gap-2 py-4 whitespace-nowrap">
-                        <button className="w-9 h-9 rounded-md bg-brand/30 text-brand flex justify-center items-center">
-                          <FaEye size={16} />
-                        </button>
-                        <button
-                          onClick={() => handleOpneModal("edit")}
-                          className="w-9 h-9 rounded-md bg-yellow/30 text-yellow flex justify-center items-center"
-                        >
-                          <AiFillEdit size={16} />
-                        </button>
-                        <button
-                          onClick={openModalDelete}
-                          className="w-9 h-9 rounded-md bg-red/30 text-red flex justify-center items-center"
-                        >
-                          <RiDeleteBin5Fill size={16} />
-                        </button>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Table columns={columns} dataSource={data} />
 
       {/* Modal Add User */}
       <AddUser
         openModalAdd={openModalAdd}
         handleCloseModal={handleCloseModal}
       />
-
       {/* Modal Edit User */}
       <EditUser
         openModalEdit={openModalEdit}
         handleCloseModal={handleCloseModal}
       />
-
       {/* Modal Details User */}
       <DetailsUser
-        modalDetailsOpened={modalDetailsOpened}
-        closeModalDetails={closeModalDetails}
+        openModalDetail={openModalDetail}
+        handleCloseModal={handleCloseModal}
       />
-
       {/* Modal Delete User */}
       <DeleteUser
-        modalDeleteOpened={modalDeleteOpened}
-        closeModalDelete={closeModalDelete}
+        openModalDelete={openModalDelete}
+        handleCloseModal={handleCloseModal}
       />
     </>
   );
