0715de570e98556ee131a1daabbd6a87bbf680a8
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-07-07 16:51:01 +0100

    Add images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: { ignoreBuildErrors: true },
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+module.exports = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  optimizeFonts: false,
+  env: {
+    // DB_CONNECTION_LOCAL: process.env.DB_CONNECTION_LOCAL,
+    // AUTH_SECRET: process.env.AUTH_SECRET,
+    // CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    // CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    // CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+  },
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+};
