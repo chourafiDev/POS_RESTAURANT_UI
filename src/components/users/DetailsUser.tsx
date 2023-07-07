bc6fd33510671b63a49d1045800a4dde61e30af0
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-07-07 16:54:39 +0100

    Show user info

--- src/components/users/DetailsUser.tsx
+++ src/components/users/DetailsUser.tsx
@@ -1,22 +1,28 @@
 "use client";
 
-// import { Input, Modal, useMantineTheme } from "@mantine/core";
-import { FC, useState } from "react";
-import Button from "../ui/Button";
-import { FileInput, rem } from "@mantine/core";
-import { FiUpload } from "react-icons/fi";
+import { FC } from "react";
 import Image from "next/image";
 import { Modal } from "antd";
+import { useGetUserByIdQuery } from "@/redux/services/userApiSlice";
 
 interface DetailsUserProps {
   openModalDetail: boolean;
   handleCloseModal: (type: string) => void;
+  userId: string;
 }
 
 const DetailsUser: FC<DetailsUserProps> = ({
   openModalDetail,
   handleCloseModal,
+  userId,
 }) => {
+  const {
+    data: user,
+    isLoading,
+    isFetching,
+    error,
+  } = useGetUserByIdQuery(userId);
+
   return (
     <Modal
       title=""
@@ -30,39 +36,41 @@ const DetailsUser: FC<DetailsUserProps> = ({
       <div className="p-4">
         <div className="bg-gray-light/60 rounded-md px-3 py-5 flex items-center gap-4">
           <Image
-            src="/assets/imgs/user.png"
+            src={
+              user?.image?.url
+                ? user?.image?.url
+                : "/assets/imgs/user-default.png"
+            }
             width={60}
             height={60}
             className="rounded-full"
-            alt="user"
+            alt="user-image"
           />
 
           <div>
-            <h3 className="text-dark font-medium text-xl">Jhon stev</h3>
-            <p className="text-dark/60">Cashier</p>
+            <h3 className="text-dark font-medium text-xl">
+              {user?.firstName} {user?.lastName}
+            </h3>
+            <p className="text-dark/60">{user?.role}</p>
           </div>
         </div>
 
         <div className="grid grid-cols-2 mt-6 gap-4">
           <div>
             <strong className="text-dark font-semibold">Emal Address</strong>
-            <p className="text-dark/80 text-[15px]">jhonstev@gmail.com</p>
+            <p className="text-dark/80 text-[15px]">{user?.email}</p>
           </div>
           <div>
             <strong className="text-dark font-semibold">Phone Number</strong>
-            <p className="text-dark/80 text-[15px]">0678987656</p>
+            <p className="text-dark/80 text-[15px]">{user?.phone}</p>
           </div>
           <div>
             <strong className="text-dark font-semibold">Role</strong>
-            <p className="text-dark/80 text-[15px]">Cashier</p>
+            <p className="text-dark/80 text-[15px]">{user?.role}</p>
           </div>
           <div>
-            <strong className="text-dark font-semibold">Zip Code</strong>
-            <p className="text-dark/80 text-[15px]">809876</p>
-          </div>
-          <div>
-            <strong className="text-dark font-semibold">Location</strong>
-            <p className="text-dark/80 text-[15px]">Location</p>
+            <strong className="text-dark font-semibold">Address</strong>
+            <p className="text-dark/80 text-[15px]">{user?.address}</p>
           </div>
         </div>
       </div>
