d76f25596feac57ce6bfa15687218eddf1746f33
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-07-09 19:20:15 +0100

    Add assets

---
+++ src/utils/assets.tsx
@@ -0,0 +1,25 @@
+import burger from "/public/assets/imgs/icons/burger.png";
+import dish from "/public/assets/imgs/icons/dish.png";
+import cake from "/public/assets/imgs/icons/cake.png";
+import dessert from "/public/assets/imgs/icons/dessert.png";
+import doughnut from "/public/assets/imgs/icons/doughnut.png";
+import meat from "/public/assets/imgs/icons/Meat.png";
+import pizza from "/public/assets/imgs/icons/pizza.png";
+import salad from "/public/assets/imgs/icons/salad.png";
+import sushi from "/public/assets/imgs/icons/sushi.png";
+import taco from "/public/assets/imgs/icons/taco.png";
+import juice from "/public/assets/imgs/icons/juice.png";
+
+export {
+  burger,
+  cake,
+  dish,
+  dessert,
+  doughnut,
+  meat,
+  pizza,
+  salad,
+  sushi,
+  taco,
+  juice
+};
