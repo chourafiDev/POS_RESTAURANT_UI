f179988e51071bd07dcb400c4a2e338256fd8f46
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-07-13 15:29:51 +0100

    Fix design and adapte function add product

--- src/components/settings/menu/AddItem.tsx
+++ src/components/settings/menu/AddItem.tsx
@@ -1,154 +1,339 @@
 "use client";
 
-import {
-  Input,
-  Modal,
-  NumberInput,
-  Textarea,
-  useMantineTheme,
-} from "@mantine/core";
-import { FC, useCallback, useState } from "react";
-import { FileInput, rem } from "@mantine/core";
-import { FiUpload } from "react-icons/fi";
-import { RxDash } from "react-icons/rx";
-import { BiPlus } from "react-icons/bi";
+import { Loader } from "@mantine/core";
+import { FC, useEffect, useState } from "react";
 import Button from "@/components/ui/Button";
+import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
+import {
+  Modal,
+  Form,
+  Input,
+  Select,
+  Row,
+  Col,
+  Upload,
+  Button as UploadButton,
+  InputNumber,
+  Button as AntdButton,
+  App,
+} from "antd";
+import { UploadOutlined } from "@ant-design/icons";
+import type { RcFile } from "antd/es/upload/interface";
+import { Product } from "../../../../types";
+import { useCreateProductMutation } from "@/redux/services/productApiSlice";
+import Image from "next/image";
+import { useGetCategoriesQuery } from "@/redux/services/categoryApiSlice";
+
+const { Option } = Select;
+
+type SizeType = Parameters<typeof Form>[0]["size"];
 
 interface AddItemProps {
-  modalAddOpened: boolean;
-  closeModalAdd: () => void;
+  openModalAdd: boolean;
+  handleCloseModal: (type: string) => void;
 }
 
-const AddItem: FC<AddItemProps> = ({ modalAddOpened, closeModalAdd }) => {
-  const theme = useMantineTheme();
+const AddItem: FC<AddItemProps> = ({ openModalAdd, handleCloseModal }) => {
+  const { message } = App.useApp();
 
-  const [options, setOptions] = useState([""]);
+  // Fetch categories
+  const { data: categories } = useGetCategoriesQuery();
 
-  const handleClick = useCallback(() => {
-    // for (let i = 0; i < options.length; i++) {
-    //   console.log("type", typeof !options[i]);
-    //   if (!options[0]) {
-    //     console.log("case 1");
-    //     setOptions([...options, ""]);
-    //   } else if (i > 0 || !options[i]) {
-    //     console.log("case 2");
-    //     setOptions([...options]);
-    //   } else {
-    //     console.log("case 3");
-    //     setOptions([...options, ""]);
-    //   }
-    // }
-    if (options.length >= 10) {
-      return;
-    } else {
-      setOptions([...options, ""]);
+  const [form] = Form.useForm();
+  const [isValideType, setIsValideType] = useState<boolean>(false);
+  const [isValideSize, setIsValideSize] = useState<boolean>(false);
+
+  // handle upload image
+  const convertToBase64 = (e: any) => {
+    const file = e.fileList[0]?.originFileObj;
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        if (reader.readyState === 2) {
+          console.log("reader.result", reader.result);
+          form.setFieldsValue({
+            image: reader.result,
+          });
+        }
+      };
+      reader?.readAsDataURL(file);
     }
-  }, [options]);
-
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement>,
-    index: number
-  ) => {
-    const { value } = e.target;
-    const onchangeVal: any = [...options];
-    onchangeVal[index] = value;
-    setOptions(onchangeVal);
   };
 
-  const handleDelete = (index: number) => {
-    const deleteVal = [...options];
-    deleteVal.splice(index, 1);
-    setOptions(deleteVal);
+  const beforeUpload = (file: RcFile) => {
+    setIsValideType(false);
+    setIsValideSize(false);
+    console.log("test");
+    const isJpgOrPng =
+      file.type === "image/jpeg" ||
+      file.type === "image/jpg" ||
+      file.type === "image/png";
+    if (!isJpgOrPng) {
+      console.log("test jpg");
+      setIsValideType(true);
+    }
+    const isLt2M = file.size / 1024 / 1024 < 2;
+    if (!isLt2M) {
+      setIsValideSize(true);
+    }
+
+    return false;
   };
+
+  const dummyRequest = (options: any) => {
+    const { onSuccess } = options;
+    setTimeout(() => {
+      onSuccess("ok");
+    }, 0);
+  };
+
+  const normFile = (e: any) => {
+    if (Array.isArray(e)) {
+      return e;
+    }
+    return e && e.fileList;
+  };
+
+  // handle create new product
+  const [addNewProduct, { isLoading, isSuccess, isError }] =
+    useCreateProductMutation();
+
+  const onFinish = async (values: Product) => {
+    try {
+      if (isValideSize) {
+        return message.error("Image must smaller than 2MB!");
+      }
+      if (isValideType) {
+        return message.error("You can only upload JPG/PNG file!");
+      }
+      await addNewProduct(values).unwrap();
+    } catch (err: any) {
+      message.error(err.data?.message || err.error);
+    }
+  };
+
+  useEffect(() => {
+    if (isSuccess) {
+      message.success("Product added successfully");
+      handleCloseModal("add");
+      form.resetFields();
+    }
+  }, [isSuccess]);
 
   return (
     <Modal
-      opened={modalAddOpened}
-      onClose={closeModalAdd}
-      withCloseButton={true}
+      title="Add New Product"
       centered
-      title="Add Item"
-      overlayProps={{
-        color:
-          theme.colorScheme === "dark"
-            ? theme.colors.dark[9]
-            : theme.colors.gray[2],
-        opacity: 0.55,
-        blur: 3,
-      }}
-      size="xl"
+      open={openModalAdd}
+      onOk={() => handleCloseModal("add")}
+      onCancel={() => handleCloseModal("add")}
+      width={1000}
+      footer={[]}
     >
-      <form className="px-4 pb-7 form">
-        <Input.Wrapper id="title" label="Title">
-          <Input id="title" />
-        </Input.Wrapper>
-        <Textarea label="Description" autosize minRows={3} className="mt-4" />
-        <NumberInput
-          label="Price"
-          defaultValue={1}
-          parser={(value) => value.replace(/\$\s?|(,*)/g, "")}
-          formatter={(value) =>
-            !Number.isNaN(parseFloat(value))
-              ? `$ ${value}`.replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")
-              : "$ "
-          }
-          className="mt-4"
-        />
+      <div className="p-4">
+        <Form
+          form={form}
+          name="add-new-product"
+          onFinish={onFinish}
+          className="w-full"
+          scrollToFirstError
+          layout="vertical"
+          autoComplete="off"
+          requiredMark={false}
+          size={"large" as SizeType}
+          initialValues={{
+            price: 0,
+          }}
+          preserve={false}
+        >
+          <div className="mb-4">
+            <Form.Item
+              name="image"
+              rules={[
+                {
+                  required: true,
+                  message: "Image is required!",
+                },
+              ]}
+              valuePropName="list"
+              getValueFromEvent={normFile}
+            >
+              <Upload
+                listType="picture"
+                customRequest={dummyRequest}
+                maxCount={1}
+                beforeUpload={beforeUpload}
+                onChange={convertToBase64}
+              >
+                <UploadButton>
+                  <div className="flex items-center gap-3">
+                    <UploadOutlined className="text-dark" />{" "}
+                    <p className="text-dark">Upload Image</p>
+                  </div>
+                </UploadButton>
+              </Upload>
+            </Form.Item>
+          </div>
 
-        <div className="bg-gray-light/40 rounded-lg my-6 p-3 border border-gray-light/70">
-          <div className="flex items-center justify-between gap-2 mb-2">
-            <label className="text-sm text-gray font-medium">Options</label>
-            {options.length >= 10 && (
-              <p className="text-sm text-brand font-medium">
-                You have 10 options maximim
-              </p>
-            )}
-            <div className="inline-block">
+          <div className="mb-4">
+            <Form.Item
+              name="title"
+              label="Title"
+              rules={[
+                {
+                  required: true,
+                  message: "Title is required!",
+                },
+              ]}
+            >
+              <Input className="w-full" />
+            </Form.Item>
+          </div>
+
+          <div className="mb-4">
+            <Form.Item
+              name="description"
+              label="Description"
+              rules={[{ required: true, message: "Description is required!" }]}
+            >
+              <Input.TextArea />
+            </Form.Item>
+          </div>
+
+          <div className="mb-4">
+            <Form.Item
+              name="category"
+              label="Category"
+              rules={[{ required: true, message: "Category is required!" }]}
+            >
+              <Select>
+                {categories?.map(({ name, icon, _id }) => (
+                  <Option value={_id} key={_id}>
+                    <div className="flex items-center gap-2">
+                      <Image
+                        src={icon?.url}
+                        alt="burger"
+                        width={20}
+                        height={20}
+                      />
+                      <p>{name}</p>
+                    </div>
+                  </Option>
+                ))}
+              </Select>
+            </Form.Item>
+          </div>
+
+          <div className="mb-4">
+            <Form.Item
+              name="price"
+              label="Price"
+              rules={[{ required: true, message: "Price is required!" }]}
+            >
+              <InputNumber addonAfter="DH" className="w-full" />
+            </Form.Item>
+          </div>
+
+          <div className="bg-brand/5 border border-brand/10 p-4 rounded-lg mt-4">
+            <label className="text-dark pb-1">Options List</label>
+            <Form.List
+              name="options"
+              rules={[
+                {
+                  validator: async (_, names) => {
+                    if (!names || names.length < 2) {
+                      return Promise.reject(new Error("At least 2 options"));
+                    }
+                  },
+                },
+              ]}
+            >
+              {(fields, { add, remove }, { errors }) => (
+                <>
+                  {fields.map((field) => (
+                    <Form.Item
+                      label={`Option ${field.key + 1}`}
+                      required={false}
+                      key={field.key}
+                    >
+                      <Form.Item
+                        {...field}
+                        key={field.key}
+                        validateTrigger={["onChange", "onBlur"]}
+                        rules={[
+                          {
+                            required: true,
+                            whitespace: true,
+                            message:
+                              "Please fill option name or delete this field.",
+                          },
+                        ]}
+                        noStyle
+                      >
+                        <div className="flex items-center gap-2">
+                          <Input />
+                          <div
+                            className=" bg-brand cursor-pointer w-8 h-8 rounded-lg text-white flex items-center justify-center"
+                            onClick={() => remove(field.name)}
+                          >
+                            <MinusCircleOutlined />
+                          </div>
+                        </div>
+                      </Form.Item>
+                    </Form.Item>
+                  ))}
+                  <Form.Item>
+                    <AntdButton
+                      type="dashed"
+                      onClick={() => add()}
+                      icon={<PlusOutlined />}
+                      className="mt-4"
+                    >
+                      Add Option
+                    </AntdButton>
+                    <Form.ErrorList errors={errors} />
+                  </Form.Item>
+                </>
+              )}
+            </Form.List>
+          </div>
+
+          <Row gutter={16} className="mt-6">
+            <Col span={12}>
               <Button
                 variant="default"
-                size="sm"
+                size="default"
                 rounded="full"
-                type="button"
-                onClick={handleClick}
+                disabled={isLoading}
+                className="gap-2"
               >
-                <BiPlus size={20} /> Add
+                {isLoading ? (
+                  <>
+                    <Loader color="#ffffff" size="xs" />
+                    <span>Add</span>
+                  </>
+                ) : (
+                  <span>Add</span>
+                )}
               </Button>
-            </div>
-          </div>
-
-          <div className="grid grid-cols-2 gap-2">
-            {options.map((val, index) => (
-              <div key={index} className="flex items-center gap-1">
-                <input
-                  name="option"
-                  value={val}
-                  onChange={(e) => handleChange(e, index)}
-                  className="input"
-                />
-                <button
-                  type="button"
-                  onClick={() => handleDelete(index)}
-                  className="bg-gray-light text-red rounded-full w-8 h-8 border border-gray/10 flex justify-center items-center"
-                >
-                  <RxDash size={20} />
-                </button>
-              </div>
-            ))}
-          </div>
-        </div>
-
-        <FileInput
-          label="Upload Image"
-          placeholder="choose image"
-          icon={<FiUpload size={rem(14)} />}
-        />
-
-        <div className="flex items-center gap-4 mt-10">
-          <Button variant="default" size="default" rounded="full">
-            Add
-          </Button>
-        </div>
-      </form>
+            </Col>
+            <Col span={12}>
+              {" "}
+              <Button
+                key="back"
+                variant="outline"
+                size="default"
+                rounded="full"
+                className="gap-2"
+                onClick={() => handleCloseModal("add")}
+              >
+                Cancel
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </div>
     </Modal>
   );
 };
