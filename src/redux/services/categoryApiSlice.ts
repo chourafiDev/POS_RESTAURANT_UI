5ee52ee30513ab6f82a83d2489ef6bd4b464485e
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-07-13 15:32:17 +0100

    Add tag types

--- src/redux/services/categoryApiSlice.ts
+++ src/redux/services/categoryApiSlice.ts
@@ -4,11 +4,15 @@ import { apiSlice } from "./apiSlice";
 const CATEGORY_URL = "api/categories";
 
 interface IUpdateCategoryData {
-  values: Category;
+  data: Category;
   categoryId: string;
 }
 
-export const categoryApiSlice = apiSlice.injectEndpoints({
+const apiSliceWithTag = apiSlice.enhanceEndpoints({
+  addTagTypes: ["Categories"],
+});
+
+export const categoryApiSlice = apiSliceWithTag.injectEndpoints({
   endpoints: (builder) => ({
     getCategories: builder.query<Category[], null>({
       query: () => {
@@ -18,9 +22,13 @@ export const categoryApiSlice = apiSlice.injectEndpoints({
           credentials: "include",
         };
       },
+      // transformResponse: (res: Category[]): Category[] =>
+      //   res.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime()),
+      providesTags: ["Categories"],
     }),
     getCategoryById: builder.query({
       query: (categoryId: string) => {
+        console.log("categoryId", categoryId);
         return {
           url: `${CATEGORY_URL}/${categoryId}`,
           method: "GET",
@@ -37,17 +45,18 @@ export const categoryApiSlice = apiSlice.injectEndpoints({
           credentials: "include",
         };
       },
+      invalidatesTags: ["Categories"],
     }),
     updateCategory: builder.mutation({
-      query: ({ values, categoryId }: IUpdateCategoryData) => {
-        console.log("data", { values, categoryId });
+      query: ({ data, categoryId }: IUpdateCategoryData) => {
         return {
           url: `${CATEGORY_URL}/${categoryId}`,
           method: "PUT",
-          body: values,
+          body: data,
           credentials: "include",
         };
       },
+      invalidatesTags: ["Categories"],
     }),
     deleteCategory: builder.mutation({
       query: (categoryId: string) => {
@@ -57,6 +66,7 @@ export const categoryApiSlice = apiSlice.injectEndpoints({
           credentials: "include",
         };
       },
+      invalidatesTags: ["Categories"],
     }),
   }),
 });
