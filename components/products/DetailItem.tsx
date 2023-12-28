ed3585d59926013b8cd971f117e35f102a3dee40
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-12-28 23:26:10 +0100

    styles: fix design

--- components/products/DetailItem.tsx
+++ components/products/DetailItem.tsx
@@ -16,7 +16,7 @@ const DetailItem: FC<DetailItemProps> = ({
   handleCloseModal,
   productId,
 }) => {
-  const { data: product, isLoading } = useGetProductByIdQuery(productId);
+  const { data: product } = useGetProductByIdQuery(productId);
 
   return (
     <Modal
@@ -29,48 +29,44 @@ const DetailItem: FC<DetailItemProps> = ({
       footer={[]}
       closable={false}
     >
-      {isLoading ? (
-        <Loading styles="h-[250px]" />
-      ) : (
-        <div className="grid grid-cols-2 gap-8 p-4">
-          <div>
-            <div className="w-full h-[250px] shadow-lg shadow-gray-light/80 relative">
-              {product?.image?.url && (
-                <Image
-                  src={product?.image?.url}
-                  alt="Tacos Salsa With Chicken"
-                  fill
-                  className="rounded-lg object-cover"
-                />
-              )}
-            </div>
-          </div>
-          <div>
-            <div className="flex justify-between items-center flex-wrap">
-              <h1 className="text-dark/90 text-[18px] font-medium tracking-wide">
-                {product?.title}
-              </h1>
-              <p className="text-brand font-medium">${product?.price}</p>
-            </div>
-
-            <p className="text-sm font-light text-gray/90 leading-6 mt-2">
-              {product?.description}
-            </p>
-
-            <h3 className="text-dark font-medium mt-4">Options</h3>
-            <div className="flex items-center gap-4 flex-wrap mt-2">
-              {product?.options?.map((el: string, index: number) => (
-                <p
-                  key={index}
-                  className="border border-gray-light/80 rounded-full px-4 py-1 text-dark"
-                >
-                  {el}
-                </p>
-              ))}
-            </div>
+      <div className="grid grid-cols-2 gap-8 p-4">
+        <div>
+          <div className="w-full h-[250px] shadow-lg shadow-gray-light/80 relative">
+            {product?.image?.url && (
+              <Image
+                src={product?.image?.url}
+                alt="Tacos Salsa With Chicken"
+                fill
+                className="rounded-lg object-cover"
+              />
+            )}
           </div>
         </div>
-      )}
+        <div>
+          <div className="flex justify-between items-center flex-wrap">
+            <h1 className="text-dark/90 text-[18px] font-medium tracking-wide">
+              {product?.title}
+            </h1>
+            <p className="text-brand font-medium">${product?.price}</p>
+          </div>
+
+          <p className="text-sm font-light text-gray/90 leading-6 mt-2">
+            {product?.description}
+          </p>
+
+          <h3 className="text-dark font-medium mt-4">Options</h3>
+          <div className="flex items-center gap-4 flex-wrap mt-2">
+            {product?.options?.map((el: string, index: number) => (
+              <p
+                key={index}
+                className="border border-gray-light/80 rounded-full px-4 py-1 text-dark"
+              >
+                {el}
+              </p>
+            ))}
+          </div>
+        </div>
+      </div>
     </Modal>
   );
 };
