05061cd04918b807fd409aeb630b488ff655156d
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-12-30 15:19:21 +0100

    refactor: fix roures

--- router/routes.ts
+++ router/routes.ts
@@ -1,2 +1,27 @@
-export const protectedRoutes = ["/en/dashboard", "/en/tables"];
-export const authRoutes = ["/en/login"];
+export const protectedRoutes = [
+  "/en/dashboard",
+  "/en/tables",
+  "/en/orders",
+  "/en/users",
+  "/en/products",
+  "/en/categories",
+  "/en/manage-tables",
+  "/en/all-history",
+  "/en/my-history",
+  "/en/menu",
+  "/en/profile",
+];
+export const authRoutes = [
+  "/en/login",
+  "/en/forgot-password",
+  "/en/reset-password",
+];
+
+export const adminRoutes = [
+  "/en/dashboard",
+  "/en/users",
+  "/en/categories",
+  "/en/products",
+  "/en/manage-tables",
+  "/en/all-history",
+];
