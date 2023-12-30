0c820ac8273dcb55152e8b368b1948a35c1c940a
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-12-30 16:49:13 +0100

    refactor: fix code

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,14 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { adminRoutes, authRoutes, protectedRoutes } from "@/router/routes";
+import { getRoutes } from "@/router/routes";
 
 export function middleware(request: NextRequest) {
   const currentUser = request.cookies.get("currentUser")?.value;
   const url = new URL(request.url);
   const pathname = url.pathname;
+  const lang = pathname.split("/")[1];
+
+  const { protectedRoutes, authRoutes, adminRoutes } = getRoutes(lang);
 
   // Check if the pathname is in authRoutes
   const isProtectedRoute = protectedRoutes.some((route) => {
@@ -13,7 +16,9 @@ export function middleware(request: NextRequest) {
   });
 
   if (isProtectedRoute && !currentUser) {
-    const response = NextResponse.redirect(new URL("/en/login", request.url));
+    const response = NextResponse.redirect(
+      new URL(`/${lang}/login`, request.url)
+    );
     response.cookies.delete("currentUser");
 
     return response;
@@ -25,7 +30,7 @@ export function middleware(request: NextRequest) {
   });
 
   if (isAuthRoute && currentUser) {
-    return NextResponse.redirect(new URL("/en/dashboard", request.url));
+    return NextResponse.redirect(new URL(`/${lang}/dashboard`, request.url));
   }
 
   // Check if the pathname is in adminRoutes (admin role)
@@ -36,6 +41,6 @@ export function middleware(request: NextRequest) {
   });
 
   if (isAdminRoute && userInfo && userInfo.role != "admin") {
-    return NextResponse.redirect(new URL("/en/denied", request.url));
+    return NextResponse.redirect(new URL(`/${lang}/denied`, request.url));
   }
 }
