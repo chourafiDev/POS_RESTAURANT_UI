ec0777f9845f8c7075ceea2c83f16e30d7e944af
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-12-30 15:32:13 +0100

    refactor: add metadata

--- app/[locale]/(app)/orders/page.tsx
+++ app/[locale]/(app)/orders/page.tsx
@@ -1,8 +1,11 @@
-"use client";
-
 import Orders from "@/components/orders/Orders";
 import OrdersCompleted from "@/components/orders/OrdersCompleted";
 import OrdersOnProcess from "@/components/orders/OrdersOnProcess";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "POS | Orders",
+};
 
 const tabs = [
   {
