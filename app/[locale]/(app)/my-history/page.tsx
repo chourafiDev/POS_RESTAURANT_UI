3f48ce5eba5b0c950ca85e22b182a9e6b8274965
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-12-30 12:51:48 +0100

    Add my history page

---
+++ app/[locale]/(app)/my-history/page.tsx
@@ -0,0 +1,11 @@
+import History from "@/components/history/History";
+
+const Page = () => {
+  return (
+    <div className="bg-white rounded-xl p-5 shadow-md shadow-gray-light/20">
+      <History />
+    </div>
+  );
+};
+
+export default Page;
