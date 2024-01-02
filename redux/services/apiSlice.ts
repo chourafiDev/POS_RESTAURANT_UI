25b7b1d5d50e270880579433415fca55a68dd8c4
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-01-02 10:54:32 +0100

    refactor: remove commented code

--- redux/services/apiSlice.ts
+++ redux/services/apiSlice.ts
@@ -1,10 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-// const baseUrl =
-//   process.env.NODE_ENV === "production"
-//     ? "process.env.NEXT_PUBLIC_API_URL"
-//     : "http://localhost:8080/";
-
 const baseQuery = fetchBaseQuery({
   baseUrl: process.env.NEXT_PUBLIC_API_URL,
 });
