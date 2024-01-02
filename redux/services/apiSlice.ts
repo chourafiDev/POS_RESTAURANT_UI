ef6da52c92a5af5c412d1dec75e6bc4cd484e8d9
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-01-02 20:53:28 +0100

    refactor: use production api

--- redux/services/apiSlice.ts
+++ redux/services/apiSlice.ts
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
+  // baseUrl: "http://localhost:8080/",
   baseUrl: process.env.NEXT_PUBLIC_API_URL,
 });
 export const apiSlice = createApi({
