34dd181cd991d9a78f5c062f9128a9c19eccbd9c
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-01-03 17:03:50 +0100

    refactor: remove tabs

--- app/[locale]/(app)/orders/page.tsx
+++ app/[locale]/(app)/orders/page.tsx
@@ -1,25 +1,10 @@
 import Orders from "@/components/orders/Orders";
-import OrdersCompleted from "@/components/orders/OrdersCompleted";
-import OrdersOnProcess from "@/components/orders/OrdersOnProcess";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "POS | Orders",
 };
 
-const tabs = [
-  {
-    title: "On-process",
-    id: "on-process",
-    content: OrdersOnProcess,
-  },
-  {
-    title: "Completed",
-    id: "completed",
-    content: OrdersCompleted,
-  },
-];
-
 export default function Order() {
-  return <Orders tabs={tabs} />;
+  return <Orders />;
 }
