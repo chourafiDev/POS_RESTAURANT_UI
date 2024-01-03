123b6d4948d9d73b060a39accb20cbc2150b5072
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-01-03 15:19:59 +0100

    refactor: remove subTotal and discount

--- components/cart/Bill.tsx
+++ components/cart/Bill.tsx
@@ -1,12 +1,11 @@
 import { FC } from "react";
 
 interface billProps {
-  subtotal: number;
-  totalPrice: number;
   itemsPrice: number;
+  qty: number;
 }
 
-const bill: FC<billProps> = ({ subtotal, totalPrice, itemsPrice }) => {
+const bill: FC<billProps> = ({ itemsPrice, qty }) => {
   return (
     <div className="bg-[#F2F3F7] rounded-lg p-6 mt-10 overflow-hidden">
       <div className="flex items-center justify-between mb-3">
@@ -14,12 +13,8 @@ const bill: FC<billProps> = ({ subtotal, totalPrice, itemsPrice }) => {
         <p className="text-dark font-medium">${itemsPrice}</p>
       </div>
       <div className="flex items-center justify-between mb-3">
-        <h4 className="text-dark/60 font-normal">Subtotal</h4>
-        <p className="text-dark font-medium">${subtotal}</p>
-      </div>
-      <div className="flex items-center justify-between mb-3">
-        <h4 className="text-dark/60 font-normal">Discount sales</h4>
-        <p className="text-dark font-medium">-$3</p>
+        <h4 className="text-dark/60 font-normal">Quantity</h4>
+        <p className="text-dark font-medium">{qty}</p>
       </div>
       <div className="border-dashed-with-spacing relative mb-6 mt-7">
         <div className="absolute -bottom-3 -left-9 w-7 h-7 rounded-full bg-white"></div>
@@ -27,7 +22,7 @@ const bill: FC<billProps> = ({ subtotal, totalPrice, itemsPrice }) => {
       </div>
       <div className="flex items-center justify-between">
         <h4 className="text-dark text-xl font-medium">Total sales</h4>
-        <p className="text-dark text-xl font-medium">${totalPrice}</p>
+        <p className="text-dark text-xl font-medium">${itemsPrice}</p>
       </div>
     </div>
   );
