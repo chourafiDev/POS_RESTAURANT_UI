fbacfd0190fc18b0b62acf03fd20416bbf128811
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-01-04 12:44:48 +0100

    refactor: add startDate and endDate

--- redux/services/orderApiSlice.ts
+++ redux/services/orderApiSlice.ts
@@ -5,9 +5,9 @@ const ORDERS_URL = "api/orders";
 export const ordersApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getOrders: builder.query({
-      query: () => {
+      query: ({ startDate, endDate }) => {
         return {
-          url: `${ORDERS_URL}`,
+          url: `${ORDERS_URL}?startDate=${startDate}&endDate=${endDate}`,
           method: "GET",
           credentials: "include",
         };
